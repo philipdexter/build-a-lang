9f8970d766d5cdbb59600cec7057edd4bcc22162
Philip Dexter <philip.dexter@gmail.com>
2014-04-26 04:31:52 -0400

    Add semantic option for delimiter

--- js.js
+++ js.js
@@ -35,7 +35,12 @@ load_lang = function() {
     a.forEach(function(v, i) {
 	obj.rules.push([v[0], encodeURIComponent($(v[1]).val() || $(v[1]).attr('placeholder'))]);
     });
+
     obj.l_name = encodeURIComponent($("#l_name").val() || $("#l_name").attr('placeholder'));
+
+    var delim = $("input[name='_SEMDELIM']:checked").val();
+    obj.delim = delim;
+
     return JSON.stringify(obj);
 }
 
