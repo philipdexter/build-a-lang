5c07c8116d5f17103ecaea0e4537d9db24d97d47
Philip Dexter <philip.dexter@gmail.com>
2014-04-26 06:43:51 -0400

    Add debug output

--- parse.py
+++ parse.py
@@ -69,6 +69,7 @@ def translate(file='hello_world.py', lang_def=None):
     python_code = python_code.replace('\\n', '\n')
 
     if lang_def['_SEMDELIM'] == 'br':
+        print('DOING DA THING')
         python_lines = python_code.splitlines()
         out_lines = []
         indentation = 0
@@ -76,11 +77,13 @@ def translate(file='hello_world.py', lang_def=None):
             if line.endswith(':'):
                 line = line[:-1] + ' {'
                 indentation += 1
+                print('adding', line)
                 out_lines.append(line)
                 continue
             if init_spaces(line)//4 < indentation:
                 indentation -= 1
                 out_lines.append(('    ' * indentation) + '}')
+            print('adding', line)
             out_lines.append(line)
         python_code = '\n'.join(out_lines)
 
