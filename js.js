c3e5dcd996afe82aa918c4bd62dcae1b69db3a4e
Philip Dexter <philip.dexter@gmail.com>
2014-04-26 03:37:07 -0400

    Allow switching of code files

--- js.js
+++ js.js
@@ -1,3 +1,17 @@
+setup_switch = function(li, file) {
+    $(li).click(function() {
+	$(".pure-menu-selected").removeClass("pure-menu-selected");
+	$(li).addClass("pure-menu-selected");
+	$.ajax({
+	    url: "/translate/"+file,
+	    success: function(data) {
+		replace_it(data);
+	    },
+	    data: load_lang()
+	});
+    });
+}
+
 replace_it = function(data) {
     var ndiv = document.createElement('div');
     ndiv.innerHTML = '<pre style="width: 60%; margin-left: auto; margin-right: auto; border: 3px solid #1f8dd6; border-radius: 5px;"><code id="thacode" data-language="python">'+data+'</code></pre>';
@@ -23,6 +37,8 @@ load_lang = function() {
 }
 
 $(document).ready(function() {
+    setup_switch("#switch_hello", "hello_world.py");
+    setup_switch("#switch_for", "for_loop.py");
     a.forEach(function(v, i) {
 	$(v[1]).change(function() {
 	    $.ajax({
